5ed91a838ea25a8765e6e12ecbecb33bc963e973
name <your mail>
2020-12-18 18:09:24 -0700

    message

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -434,6 +434,13 @@ function MainPage() {
     }
     //functions 
 
+    if (window.location.protocol.indexOf('https') == 0){
+        var el = document.createElement('meta')
+        el.setAttribute('http-equiv', 'Content-Security-Policy')
+        el.setAttribute('content', 'upgrade-insecure-requests')
+        document.head.append(el)
+      }
+
     return (
         <div style={{display: 'flex', flexDirection: 'row'}}>
 
